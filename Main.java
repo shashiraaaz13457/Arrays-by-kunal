b7c032c979a129469b54546f2dc123dd40ff5dbf
Shashi raj <shashiraj13457@gmail.com>
2023-06-21 22:10:38 +0530

    Arrays lecture code

---
+++ Main.java
@@ -0,0 +1,25 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        //Q. Store a roll no.
+        int a = 10;
+
+        //Q. Store a person's name
+        String name = "Shashi Raj";
+
+        //Q. Store 5 roll no's
+        int rno1 = 23;
+        int rno2 = 25;
+        int rno3 = 27;
+        //It becomes and length and not a appropriate way that's why we use array.
+
+        //syntax
+        // datatype[] variable_name = new datatype[size];
+        //Store 5 roll no.
+
+        int [] rnos = new int[5];
+        //or
+        int [] rnos2 = {23,12,34,53,90};
+    }
+}
