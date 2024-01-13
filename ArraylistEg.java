a1fc37ec3b008729d209bec21d89d0ad9037ddef
Shashi raj <shashiraj13457@gmail.com>
2024-01-13 22:45:53 +0530

    Arraylist

---
+++ ArraylistEg.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ArraylistEg {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        //Syntax
+        ArrayList<Integer> List = new ArrayList<>(10); // This doesn't mean it will store only 10 numbers.
+
+//        List.add(67);
+//        List.add(76);
+//        List.add(76);
+//        List.add(76);
+//        List.add(88);
+//        List.add(76);
+//        List.add(76);
+//        List.add(76);
+
+
+//        System.out.println(List.contains(67)); //true.
+//        List.set(0,80); // updating the value of Arraylist.
+//        List.remove(4);
+//        System.out.println(List);  //Printing the list.
+
+        //input
+        for (int i = 0; i < 5; i++) {
+            List.add(input.nextInt());
+        }
+        //get item at any index
+        
+//        for (int i = 0; i < 5; i++) {
+//            System.out.println(List.get(i));
+//        }
+
+//        System.out.println(List);
+    }
+}
---
+++ Leetcode.java
@@ -0,0 +1,28 @@
+//class Leetcode {
+//    public int reverse(int x) {
+//        int rem = 0;
+//        int sum = 0;
+//        int temp = x;
+//        while (x > Integer.MIN_VALUE){
+//
+//            rem = x % 10;
+//            sum = sum * 10 + rem;
+//            x = x/10;
+//        }
+//        if (temp == sum){
+////            System.out.println(sum);
+//            return sum;
+//        }
+//
+//        return sum;
+//    }
+//}
+
+
+//Ques 2. Pascal's Triangle
+
+//class Solution {
+//    public List<List<Integer>> generate(int numRows) {
+//
+//    }
+//}
